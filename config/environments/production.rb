d5c38e6a35715bf8482680005fd4166ed56c820e
Jimmy FRAGNE <fragnejimmy@gmail.com>
2018-01-13 11:55:30 +0100

    change name furnitroc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "furnitour_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "furnitroc_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
