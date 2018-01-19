7dc4deededb4d775cf1386b05fe75565fc6eb728
Jimmy FRAGNE <fragnejimmy@gmail.com>
2018-01-19 22:43:10 +0100

    inscription site

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/home'
+
+  get 'users/login'
+
   get 'retirer' ,to: 'pages#retirer' , as: :retirer
   get 'deposer' ,to: 'pages#deposer' , as: :deposer
 
   root 'pages#home'
+
+  get 'users/home' => 'users#home'
+  get 'users/login' => 'users#login'
+  post 'users/login' => 'users#check'
 
 end
